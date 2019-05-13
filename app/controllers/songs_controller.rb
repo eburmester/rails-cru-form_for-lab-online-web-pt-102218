a54c33b7684e793cc75358ff84c6d244bc0fd3ab
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-13 19:50:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
   def create 
     @song = Song.new(song_params)
     @song.save 
-    redirect_to song_path(params)
+    redirect_to song_path(@song)
   end 
   
   def edit 
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
   def update 
     @song = Song.find(params[:id])
     @song.update(song_params)
-    redirect_to song_path(params)
+    redirect_to song_path(@song)
   end 
   
   private 
