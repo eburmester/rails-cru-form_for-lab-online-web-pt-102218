ee69c5997f2cac80d6251559746fcf7c526b9423
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-13 19:50:15 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,13 +13,13 @@ class ArtistsController < ApplicationController
   def create 
     @artist = Artist.new(artist_params)
     @artist.save 
-    redirect_to artist_path(params)
+    redirect_to artist_path(@artist)
   end 
   
   def update 
     @artist = Artist.find(params[:id])
     @artist.update(artist_params)
-    redirect_to artist_path(params)
+    redirect_to artist_path(@artist)
   end 
   
   private 
