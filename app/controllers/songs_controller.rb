5e5f9436e6c6c33ce0c3a554eb660fe9bb1d9afe
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-13 19:39:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,31 @@
 class SongsController < ApplicationController
   
   def index 
+    @songs = Song.all 
   end 
   
   def create 
+    @song = Song.new(song_params)
+    @song.save 
+    redirect_to song_path(params)
+  end 
+  
+  def edit 
   end 
   
   def show 
+    @song = Song.find(params[:id])
   end 
   
-  def new 
+  def update 
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    redirect_to song_path(params)
   end 
   
+  private 
+  
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end 
 end
