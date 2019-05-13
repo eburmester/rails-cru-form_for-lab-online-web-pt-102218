b3193d16bab2ef93107650381e94eb08e409aec2
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-13 19:51:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,10 @@ class SongsController < ApplicationController
     @songs = Song.all 
   end 
   
+  def new 
+    @song = Song.new 
+  end 
+  
   def create 
     @song = Song.new(song_params)
     @song.save 
